03134e87e97cfaa26b78c18f62241328b40a4800
1363v4 <louisemmanuelgires@gmail.com>
2025-01-26 14:30:54 +0100

    grid rework

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,16 @@
 const root = document.documentElement;
 const notes = $$(".note");
 const scale = $('#scale');
+const container = $('#container');
 
 const scale1 = ["C", "F", "A#", "D#", "G#", "C#", "F#", "B", "E", "A", "D", "G"];
-const scale2 = ["lydien", "majeur", "myxolidien", "dorien", "mineur", "phrygien", "locrien"];
+const scale2 = ["Lydian", "Ionian", "Mixolydian", "Dorian", "Aeolian", "Phrygian", "Locrian"];
 
 var notesArray = [11, 0, 1, 2, 3, 4, 5];
 var turn = 0;
 var scalePointer = 1
 const screenMin = Math.min(screen.height, screen.width);
 const offset = screenMin * 0.5;
-
-notes.forEach(note => {
-    note.style.setProperty(
-        'margin-top', `calc(${offset}px * sin(((var(--turn) + ${note.dataset.note}turn - 3turn)/12)))`
-    );
-    note.style.setProperty(
-        'margin-left', `calc(${offset}px * cos(((var(--turn) + ${note.dataset.note}turn - 3turn)/12)))`
-    );
-    note.style.setProperty(
-        'rotate', `calc((var(--turn) + ${note.dataset.note}turn)/12)`
-    );
-});
 
 function update() {
     notes.forEach(note => {
@@ -42,16 +31,16 @@ function update() {
         default:
             $('#m-minus').disabled = false;
             $('#m-plus').disabled = false;
-    }
+        }
     root.style.setProperty('--mood', scalePointer);
 };
-
+    
 update();
-
+    
 click('#plus', function() {
     turn = turn + 1;
     root.style.setProperty('--turn', turn + "turn");
-    notesArray = notesArray.map(note => (note - 1 + 12) % 12);
+    notesArray = notesArray.map(note => (note + 11) % 12);
     update()
 });
 
@@ -70,6 +59,6 @@ click('#m-plus', function() {
 
 click('#m-minus', function() {
     scalePointer += 1;
-    notesArray = notesArray.map(note => (note - 1 + 12) % 12);
+    notesArray = notesArray.map(note => (note + 11) % 12);
     update()
 });
